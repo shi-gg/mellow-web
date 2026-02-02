67fed266408c243ff35b543d314d0523e39325cb
Luna <luna@wamellow.com>
2026-02-02 01:34:30 +0100

    chore: use env var for plausible api host

--- next.config.ts
+++ next.config.ts
@@ -27,11 +27,11 @@ const nextConfig: NextConfig = {
     rewrites: () => [
         {
             source: "/static/analytics.js",
-            destination: "http://172.17.0.1:7004/js/script.outbound-links.js"
+            destination: `${process.env.INTERNAL_PLAUSIBLE_API || "http://172.17.0.1"}:7004/js/script.outbound-links.js`
         },
         {
             source: "/api/event",
-            destination: "http://172.17.0.1:7004/api/event"
+            destination: `${process.env.INTERNAL_PLAUSIBLE_API || "http://172.17.0.1"}:7004/api/event`
         }
     ],
     images: {
