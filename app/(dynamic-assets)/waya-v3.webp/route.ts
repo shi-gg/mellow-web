4a2885ac46c6a2150292727e70fbeb6423d3b441
Luna Seemann <luna@wamellow.com>
2026-04-25 22:09:40 +0200

    fix: dynamically set content-type for avatar routes

--- app/(dynamic-assets)/waya-v3.webp/route.ts
+++ app/(dynamic-assets)/waya-v3.webp/route.ts
@@ -5,18 +5,19 @@ export const revalidate = 691_200; // 8 days
 export async function GET() {
     const user = await getUser(process.env.NEXT_PUBLIC_CLIENT_ID as string);
 
-    const avatar = await fetch(user?.avatar
+    const res = await fetch(user?.avatar
         ? `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp?size=256`
         : "https://cdn.discordapp.com/embed/avatars/5.png"
-    )
-        .then((r) => r.arrayBuffer());
+    );
+    const avatar = await res.arrayBuffer();
+    const contentType = res.headers.get("Content-Type") ?? "application/octet-stream";
 
     return new Response(
         avatar,
         {
             headers: {
                 "Cache-Control": "public, s-maxage=691200, immutable",
-                "Content-Type": "image/webp"
+                "Content-Type": contentType
             }
         }
     );
