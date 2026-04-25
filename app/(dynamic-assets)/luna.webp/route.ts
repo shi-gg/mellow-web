a235757fc3fa5cd52577bd6c7b46c1acd1a21842
Luna Seemann <luna@wamellow.com>
2026-04-25 22:29:03 +0200

    feat: add content-type to dynamic asset routes (#69)
    * feat: add content-type to dynamic asset routes
    
    * fix: dynamically set content-type for avatar routes
    
    * style: remove eof new line

--- app/(dynamic-assets)/luna.webp/route.ts
+++ app/(dynamic-assets)/luna.webp/route.ts
@@ -5,17 +5,19 @@ export const revalidate = 691_200; // 8 days
 export async function GET() {
     const user = await getUser("821472922140803112");
 
-    const avatar = await fetch(user?.avatarUrl
+    const res = await fetch(user?.avatarUrl
         ? user.avatarUrl + "?size=256"
         : "https://cdn.discordapp.com/embed/avatars/5.png"
-    )
-        .then((r) => r.arrayBuffer());
+    );
+    const avatar = await res.arrayBuffer();
+    const contentType = res.headers.get("Content-Type") ?? "application/octet-stream";
 
     return new Response(
         avatar,
         {
             headers: {
-                "Cache-Control": "public, s-maxage=691200, immutable"
+                "Cache-Control": "public, s-maxage=691200, immutable",
+                "Content-Type": contentType
             }
         }
     );
