267f4c4c2788ee78c3a08b5c217035c37c28aa4c
Luna Seemann <luna@wamellow.com>
2026-06-15 17:11:44 +0200

    fix: improve image placehodler previews (#82)
    * fix: improve image placehodler previews
    
    * style: add bg to fotoer icon
    
    * fix: apply changes to author icon

--- components/discord/message-embed.tsx
+++ components/discord/message-embed.tsx
@@ -4,7 +4,8 @@ import React from "react";
 
 import { DiscordMarkdown } from "./markdown";
 
-const PLACEHOLDER_REGEX = /^{(user|guild|creator)\.(icon|avatar)}$/;
+const PLACEHOLDER_REGEX = /^{(?:(?:user|guild|creator)\.(?:icon|avatar)|creator\.user)}$/;
+const STREAM_GAME_THUMBNAIL_REGEX = /^{stream\.game\.thumbnail}$/;
 const VIDEO_THUMBNAIL_REGEX = /^{(video|stream)\.thumbnail}/;
 
 interface Props {
@@ -63,7 +64,7 @@ export default function DiscordMessageEmbed({
                                 "font-semibold text-semibold mb-2 flex gap-2 items-center"
                             )}
                         >
-                            {author.icon_url && <img src={author.icon_url} alt="" className="rounded-full h-6 w-6" />}
+                            {author.icon_url && <img src={replaceTemplatesToUrl(author.icon_url)} alt="" className="rounded-full size-6 bg-wamellow" width={24} height={24} decoding="async" />}
                             <DiscordMarkdown
                                 mode={mode}
                                 text={author.text}
@@ -91,14 +92,14 @@ export default function DiscordMessageEmbed({
                     </div>
                 </div>
 
-                {thumbnail && <img src={replaceTemplatesToUrl(thumbnail)} alt="" className="ml-auto h-20 w-20 rounded-md" />}
+                {thumbnail && <img src={replaceTemplatesToUrl(thumbnail)} alt="" className="ml-auto w-20 rounded-md bg-wamellow" width={80} height={80} loading="lazy" decoding="async" />}
             </div>
 
-            {image && <img src={replaceTemplatesToUrl(image)} alt="" className="ml-auto rounded-md h-full w-full mt-4" />}
+            {image && <img src={replaceTemplatesToUrl(image)} alt="" className="rounded-md h-full w-full mt-4 bg-wamellow" width={400} height={225} loading="lazy" decoding="async" />}
 
             {footer?.text &&
                 <div className="flex gap-1 items-center mt-3">
-                    {footer.icon_url && <img src={footer.icon_url} alt="" className="rounded-full h-5 w-5" />}
+                    {footer.icon_url && <img src={replaceTemplatesToUrl(footer.icon_url)} alt="" className="rounded-full size-5 bg-wamellow" width={20} height={20} loading="lazy" decoding="async" />}
                     <span className="text-xs">
                         <DiscordMarkdown
                             mode={mode}
@@ -114,8 +115,9 @@ export default function DiscordMessageEmbed({
 }
 
 function replaceTemplatesToUrl(input: string) {
-    if (PLACEHOLDER_REGEX.test(input)) return "https://cdn.discordapp.com/embed/avatars/0.png";
-    if (VIDEO_THUMBNAIL_REGEX.test(input)) return "/_next/image?url=/notifications-thumbnail-placeholder.webp&w=384&q=75";
+    if (PLACEHOLDER_REGEX.test(input)) return "/_next/image?url=/discord.webp&w=828&q=75";
+    if (STREAM_GAME_THUMBNAIL_REGEX.test(input)) return "/_next/image?url=https://static-cdn.jtvnw.net/ttv-static/404_boxart-188x250.jpg&w=256&q=75";
+    if (VIDEO_THUMBNAIL_REGEX.test(input)) return "/_next/image?url=https://static-cdn.jtvnw.net/ttv-static/404_preview-1920x1080.jpg&w=828&q=75";
 
     if (!input.startsWith("http")) return;
     return input;
