164b43837365443babf5fd03e7a0c8f902a137ed
Luna Seemann <luna@wamellow.com>
2026-06-24 04:02:47 +0200

    refactor: clean up screen message error (#86)

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
     return (
         <ScreenMessage
             title="Sadly, this page can not be found.."
-            description="Seems like you got a little lost here? Here's wumpus for now!"
+            description="Seems like you got a little lost here?"
         />
     );
 }
