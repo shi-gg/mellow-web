265a69fbe35fbd4b2289887602bc68d417caee39
Luna Seemann <luna@wamellow.com>
2026-06-29 03:18:45 +0200

    refactor(StatusBar): move logic from JS to CSS

--- components/counter.tsx
+++ components/counter.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/utils/cn";
 import Link from "next/link";
 import { useCallback, useEffect, useRef, useState } from "react";
 import CountUp, { type CountUpProps } from "react-countup";
@@ -48,41 +49,30 @@ interface Options {
 }
 
 export function StatsBar(options: Options) {
-    const [width, setWidth] = useState(0);
-    const ref = useRef<NodeJS.Timeout | null>(null);
-
-    useEffect(() => {
-        ref.current = setInterval(setWidth, 1_000, window.innerWidth);
-        return () => {
-            if (ref.current) clearInterval(ref.current);
-        };
-    }, []);
-
     return (
         <Box
             none
-            className="grid w-full rounded-md overflow-hidden divide-x divide-wamellow"
-            style={{ gridTemplateColumns: `repeat(${width > 768 ? options.items.length : 2}, minmax(0, 1fr))` }}
+            className="flex w-full rounded-md overflow-hidden divide-x divide-wamellow"
         >
-            {options.items.slice(0, width > 768 ? 10 : 2).map((item) => (
-                <div className="p-5 dark:bg-wamellow bg-wamellow-100" key={"counter" + item.name + item.number.toString() + item.number.toString()}>
+            {options.items.map((item, i) => (
+                <div className={cn("p-5 dark:bg-wamellow bg-wamellow-100 flex-1 min-w-0", i >= 2 && "hidden md:block")} key={"counter" + item.name}>
 
                     <div className="flex">
                         <div className="text-sm font-medium mb-1">{item.name}</div>
-                        {item.info && <Link href={item.info} className="ml-auto dark:text-neutral-400 text-neutral-600 dark:hover:text-violet-400 hover:text-violet-600 duration-300">
-                            <HiInformationCircle />
-                            <span className="sr-only">information about this card</span>
-                        </Link>}
+                        {item.info && (
+                            <Link href={item.info} className="ml-auto dark:text-neutral-400 text-neutral-600 dark:hover:text-violet-400 hover:text-violet-600 duration-300">
+                                <HiInformationCircle />
+                                <span className="sr-only">information about this card</span>
+                            </Link>
+                        )}
                     </div>
 
                     <div className="md:flex">
                         <ClientCountUp className="text-3xl dark:text-neutral-100 text-neutral-900 font-medium" end={item.number} />
                         <div className="text-lg dark:text-violet-400 text-violet-600 font-medium relative md:top-2.5 md:ml-2">
                             {typeof item.gained === "number"
-                                ?
-                                <>+<ClientCountUp end={item.gained} /> {item.append || "today"}</>
-                                :
-                                item.gained
+                                ? <>+<ClientCountUp end={item.gained} /> {item.append || "today"}</>
+                                : item.gained
                             }
                         </div>
                     </div>
