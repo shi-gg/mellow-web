085f33d0d071a557a375bc6d61c7d4acccef0b35
Luna Seemann <luna@wamellow.com>
2026-06-30 17:00:14 +0200

    style(team): compact donors

--- app/(home)/team/page.tsx
+++ app/(home)/team/page.tsx
@@ -1,3 +1,5 @@
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/utils/cn";
 import { filterDuplicates } from "@/utils/filter-duplicates";
 import { getBaseUrl, getCanonicalUrl } from "@/utils/urls";
 import type { Metadata } from "next";
@@ -22,7 +24,7 @@ function formatTeamName(team: string) {
 
 export const generateMetadata = (): Metadata => {
     const title = "Team";
-    const description = "Meet the creators of Wamellow and its products. Our dedicated team, including developers and donors, drives innovation and community growth.";
+    const description = "Meet the creators of Wamellow and its products. Our dedicated team, including developers, translators and donors, drives innovation.";
     const url = getCanonicalUrl("team");
 
     return {
@@ -71,7 +73,7 @@ export default async function Home() {
         <div>
             <h2 className="text-2xl font-medium text-neutral-200">Team 👋</h2>
             <div className="max-w-xl mt-1 mb-2">
-                Meet the creators of Wamellow and its products. Our dedicated team, including developers and donors, drives innovation and community growth.
+                Meet the creators of Wamellow and its products. Our dedicated team, including developers, translators and donors, drives innovation.
             </div>
 
             <div className="relative mb-10">
@@ -80,18 +82,23 @@ export default async function Home() {
                         key={team}
                         className="py-3"
                     >
-                        <h3 className="text-lg font-medium text-neutral-200">
+                        <h3 className="text-lg font-medium text-neutral-200 flex items-center gap-2">
                             {formatTeamName(team)}
+                            <Badge radius="rounded">
+                                {members.filter((member) => member.team === team).length}
+                            </Badge>
                         </h3>
 
-                        <div className="mt-2 flex flex-wrap gap-3">
+                        <div className={cn("mt-2 flex flex-wrap gap-3", team === TeamType.Donator && "gap-2")}>
                             {members
                                 .filter((member) => member.team === team)
+                                .sort((a, b) => Number(BigInt(a.id) - BigInt(b.id)))
                                 .map((member) => (
                                     <Person
                                         key={member.id}
                                         id={member.id}
                                         social={"social" in member ? member.social : undefined}
+                                        compact={team === TeamType.Donator}
                                     />
                                 ))
                             }
@@ -123,6 +130,6 @@ export default async function Home() {
                 <DiscordServer guildId="828676951023550495" />
             </div>
 
-        </div>
+        </div >
     );
 }
--- app/(home)/team/person.component.tsx
+++ app/(home)/team/person.component.tsx
@@ -9,18 +9,34 @@ type LinkProps = DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTML
 
 export async function Person({
     id,
-    social
+    social,
+    compact
 }: {
     id: string;
     social?: string;
+    compact?: boolean;
 }) {
     const user = await getUser(id);
     if (!user) return;
 
-    const avatarUrl = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.${user.avatar?.startsWith("a_") ? "gif" : "webp"}?size=64`;
+    const avatarUrl = user.avatar
+        ? `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.${user.avatar?.startsWith("a_") ? "gif" : "webp"}?size=64`
+        : "/discord.webp";
+
+    if (compact) {
+        return (
+            <Image
+                alt={user.username}
+                className="rounded-full shrink-0 aspect-square size-10"
+                height={48}
+                src={avatarUrl}
+                width={48}
+            />
+        );
+    }
 
     return (
-        <Component
+        <Linkish
             className={cn(
                 "flex items-center gap-3 h-14 px-2 pl-1.25 pr-4 hover:bg-wamellow rounded-full cursor-default",
                 social && "duration-100 outline-violet-500 hover:outline-solid cursor-pointer"
@@ -40,7 +56,7 @@ export async function Person({
                 <div className="text-lg text-neutral-200 font-semibold -mb-1.5">{user.globalName || user.username}</div>
                 <span className="text-muted-foreground">{user.username}</span>
             </div>
-        </Component>
+        </Linkish>
     );
 }
 
@@ -48,7 +64,7 @@ function isLinkProps(props: DivProps | LinkProps): props is LinkProps {
     return "href" in props && Boolean(props.href);
 }
 
-function Component(props: DivProps | LinkProps) {
+function Linkish(props: DivProps | LinkProps) {
     if (isLinkProps(props)) {
         return <Link href={props.href as string} {...props}>{props.children}</Link>;
     }
